6586bdfea864e894639a5e05f617cc0909adcb09
reyreavman <radmirkhurum@gmail.com>
2024-10-14 21:07:31 +0700

    feature: imports cleaned up

--- src/main/java/ru/rrk/core/service/SellerService.java
+++ src/main/java/ru/rrk/core/service/SellerService.java
@@ -1,18 +1,10 @@
 package ru.rrk.core.service;
 
-import org.springframework.data.domain.Page;
 import ru.rrk.api.dto.seller.CreateSellerDTO;
 import ru.rrk.api.dto.seller.PagedSellerDTO;
 import ru.rrk.api.dto.seller.SellerDTO;
 import ru.rrk.api.dto.seller.UpdateSellerDTO;
 
-/*
-Список всех продавцов
-Инфо о конкретном продавце
-Создать нового продавца
-Обновить инфо о продавце
-Удалить продавца
- */
 public interface SellerService {
     PagedSellerDTO findAllSellers(int page, int pageSize);
 

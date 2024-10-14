1170b170a9eebc2b5f2766c80cc9627fc60e275a
reyreavman <radmirkhurum@gmail.com>
2024-10-14 21:03:49 +0700

    feature: seller mapper configured

---
+++ src/main/java/ru/rrk/core/mapper/SellerMapper.java
@@ -0,0 +1,41 @@
+package ru.rrk.core.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+import ru.rrk.api.dto.seller.CreateSellerDTO;
+import ru.rrk.api.dto.seller.PagedSellerDTO;
+import ru.rrk.api.dto.seller.SellerDTO;
+import ru.rrk.api.dto.seller.UpdateSellerDTO;
+import ru.rrk.core.entity.Seller;
+
+
+@Mapper(componentModel = "spring",
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Component
+public interface SellerMapper {
+    SellerMapper INSTANCE = Mappers.getMapper(SellerMapper.class);
+
+    SellerDTO mapToSellerDTO(Seller seller);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "registrationDate", ignore = true)
+    Seller mapToSeller(CreateSellerDTO createSellerDTO);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "registrationDate", ignore = true)
+    Seller updateSellerData(UpdateSellerDTO updateSellerDTO, @MappingTarget Seller seller);
+
+    @Mapping(target = "pageSize", source = "size")
+    @Mapping(target = "pageNumber", source = "pageable.pageNumber")
+    @Mapping(target = "totalPages", source = "totalPages")
+    @Mapping(target = "totalElements", source = "totalElements")
+    @Mapping(target = "items", source = "content")
+    PagedSellerDTO mapToPagedSellerDTO(Page<Seller> sellers);
+}

7d35c64909ba80407acbea3c5cf4a833da2c9249
reyreavman <radmirkhurum@gmail.com>
2024-10-14 21:04:43 +0700

    feature: transaction entity created

---
+++ src/main/java/ru/rrk/core/entity/transaction/Transaction.java
@@ -0,0 +1,44 @@
+package ru.rrk.core.entity.transaction;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import ru.rrk.core.entity.Seller;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+
+    private Float amount;
+
+    @Enumerated(value = EnumType.STRING)
+    private PaymentType paymentType;
+
+    @CreationTimestamp
+    private LocalDateTime transactionDate;
+}

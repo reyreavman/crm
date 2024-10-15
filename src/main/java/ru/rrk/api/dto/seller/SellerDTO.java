6ad8ef31e0a16f5a211398bb48fe491e8d4554fd
reyreavman <radmirkhurum@gmail.com>
2024-10-15 22:45:38 +0700

    feature: fields documented

--- src/main/java/ru/rrk/api/dto/seller/SellerDTO.java
+++ src/main/java/ru/rrk/api/dto/seller/SellerDTO.java
@@ -1,8 +1,14 @@
 package ru.rrk.api.dto.seller;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Данные продавца")
 public record SellerDTO(
+        @Schema(description = "Идентификатор пользователя")
         Long id,
+        @Schema(description = "Имя продавца", example = "Иван Иванов")
         String name,
+        @Schema(description = "Контактные данные", example = "+79999999999")
         String contactInfo
 ) {
 }
--- src/main/java/ru/rrk/api/dto/seller/UpdateSellerDTO.java
+++ src/main/java/ru/rrk/api/dto/seller/UpdateSellerDTO.java
@@ -1,7 +1,12 @@
 package ru.rrk.api.dto.seller;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Данные необходимые для обновления существующих данных продавца")
 public record UpdateSellerDTO(
+        @Schema(description = "Имя продавца", example = "Иван Иванов", nullable = true)
         String name,
+        @Schema(description = "Контактные данные", example = "+79999999999", nullable = true)
         String contactInfo
 ) {
 }

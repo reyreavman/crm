d080359a2a189420f824b9bfe498b1077eaa30cc
reyreavman <radmirkhurum@gmail.com>
2024-10-16 21:06:58 +0700

    feature: openAPIDefinition added

--- src/main/java/ru/rrk/Main.java
+++ src/main/java/ru/rrk/Main.java
@@ -1,8 +1,19 @@
 package ru.rrk;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "CRM API", description = "Спецификация API"
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "URL по умолчанию")
+        }
+)
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {

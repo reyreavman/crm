737761844d61a674d93067d5b49e88c4e24d67a3
reyreavman <radmirkhurum@gmail.com>
2024-10-20 02:04:32 +0700

    feature: context tests created

---
+++ src/test/java/ru/rrk/CrmApplicationTest.java
@@ -0,0 +1,12 @@
+package ru.rrk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CrmApplicationTest {
+    @Test
+    void contextLoads() {
+
+    }
+}

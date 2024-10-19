e7f424da1d449cddf89f61c296806fa33af4ef8a
reyreavman <radmirkhurum@gmail.com>
2024-10-20 01:59:30 +0700

    feature: repositories contracts updated

--- src/main/java/ru/rrk/core/repository/SellerRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.rrk.core.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import ru.rrk.core.entity.Seller;
-
-public interface SellerRepository extends JpaRepository<Seller, Long>, PagingAndSortingRepository<Seller, Long> {
-}

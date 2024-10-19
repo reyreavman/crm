4c8d18c7c05173c42716bcb2c58938446f12fc0d
reyreavman <radmirkhurum@gmail.com>
2024-10-20 02:04:19 +0700

    feature: postgres test container prop file created

---
+++ src/test/java/ru/rrk/testContainter/PostgresqlContainerTest.java
@@ -0,0 +1,32 @@
+package ru.rrk.testContainter;
+
+import org.junit.AfterClass;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Testcontainers
+public class PostgresqlContainerTest {
+    @Container
+    private static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:16")
+            .withDatabaseName("crmTests")
+            .withUsername("crmTests")
+            .withPassword("crmTests");
+
+    public static PostgreSQLContainer getInstance() {
+        return postgres;
+    }
+
+    @AfterClass
+    public static void stopContainer() {
+        postgres.stop();
+    }
+
+    @Test
+    public void test() {
+        assertTrue(postgres.isRunning());
+    }
+}

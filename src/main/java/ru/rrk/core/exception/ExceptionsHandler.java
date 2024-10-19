8fd6b68f14c0fda9b91c96b891718e79866c488c
reyreavman <radmirkhurum@gmail.com>
2024-10-20 02:02:36 +0700

    feature: exceptions handler configured

--- src/main/java/ru/rrk/core/exception/ExceptionsHandler.java
+++ src/main/java/ru/rrk/core/exception/ExceptionsHandler.java
@@ -1,11 +1,17 @@
 package ru.rrk.core.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.rrk.api.dto.error.ServiceErrorDTO;
+import ru.rrk.api.dto.error.ValidationErrorDTO;
 import ru.rrk.core.exception.service.ServiceException;
+import ru.rrk.core.exception.violation.Violation;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class ExceptionsHandler {
@@ -13,5 +19,30 @@ public class ExceptionsHandler {
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ServiceErrorDTO handleServiceException(ServiceException e) {
         return new ServiceErrorDTO(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ValidationErrorDTO handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<Violation> violations = e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(
+                        error -> new Violation(error.getField(), error.getDefaultMessage())
+                )
+                .toList();
+        return new ValidationErrorDTO(violations);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ValidationErrorDTO handleConstraintViolationException(ConstraintViolationException e) {
+        List<Violation> violations = e.getConstraintViolations()
+                .stream()
+                .map(
+                        error -> new Violation(
+                                error.getInvalidValue().toString(), error.getMessage())
+                )
+                .toList();
+        return new ValidationErrorDTO(violations);
     }
 }
